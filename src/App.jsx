0b446c38864c00235b73add7670f2c48c1855577
juliorivasz <julioandresrivas@gmail.com>
2023-02-13 19:44:48 -0300

    first commit api

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import imagenRickMorty from './img/rick-morty.png'
+import './App.css'
+import Characters from './components/characters';
+
+function App() {
+
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+    setCharacters(characterApi.results);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+        {/* se le coloca un ternario para no mostrar el inicio cuando busca el personaje */}
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <img src={imagenRickMorty} alt="Rick & Morty" className="img-home" />
+            <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+          </>
+        )}
+      </header>
+    </div>
+  )
+}
+
+export default App
